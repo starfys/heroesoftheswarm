d75e51a890fcfff81165f759bff73e60909ec109
bkul <nazekthetweaker@gmail.com>
2018-01-27 15:29:11 -0600

    Merge work on rendering and viewport scrolling (#2)
    * Basic layout and restructuring of files
    
     - Switched to Bootstrap 4
     - Created prototype layout
     - Placed CSS and JS in separate files
     - Modularized existing JS and created a main function which is called after the canvas loads
     - Switched indentation in JS to four spaces
    
    * Removed dead code from index.html
    
    * Initial netcode work and vector object
    
     - Created pingServer function which loops automatically to request new data
     - Created getState function which returns the global state without letting the requester touch it
     - Created vec2 object which will be useful for rendering
    
    * Stable framerate, server data streaming, perfunctory rendering, viewport scrolling
    
     - Added code to stabilize framerate within main game loop
     - Added code to draw particles and render data from the server
     - Added code to scroll the viewport along with the center of the client's swarm
     - Added miscellaneous helper functions
     - Added more functionality to vec2

--- game.js
+++ game.js
@@ -3,12 +3,47 @@ const PROTOCOL = "heroesoftheswarm";
 const FPS = 30;
 const PINGS_PER_FRAME = 1;
 var GLOBAL_STATE_NO_TOUCH;
+var GLOBAL_ID_NO_TOUCH;
+const PING_MODE = 'U';
 
-function main() {
-    var ws = initializeWebSocket(SERVER_URL, PROTOCOL, function(event) {GLOBAL_STATE_NO_TOUCH = event.data;});
-    setTimeout(pingServer, 500, ws);
+function init() {
+    var ws = initializeWebSocket(SERVER_URL, PROTOCOL, function(event) {
+        data = JSON.parse(JSON.parse(JSON.stringify(event.data)));
+        type = data.mt;
+        switch(type) {
+            case 'c':
+                GLOBAL_ID_NO_TOUCH = data.message.config.player_id; break;
+            case 'w':
+                GLOBAL_STATE_NO_TOUCH = data.message.world; break;
+                console.log(GLOBAL_STATE_NO_TOUCH);
+        }
+    });
     var ctx = initializeCanvas();
-    drawCircle(ctx);
+    setTimeout(pingServer, 1000, ws, ctx);
+    setTimeout(_loop, 1500, ctx, 1000 / FPS, 0);
+    $("#upload-button").on('click', function(event) {
+        ws.send(JSON.stringify({program: $("#code").val()}));
+        console.log("program sent!");
+    });
+}
+
+function frame(ctx, dt, frameN) {
+    mapSize = new vec2(1600, 900);
+    screenSize = new vec2(ctx.canvas.width, ctx.canvas.height);
+    state = getState();
+    viewport = getViewport(ctx);
+    ctx.clearRect(0, 0, screenSize.x, screenSize.y);
+    $.each(state.swarms, function(id, swarm) {
+        red = swarm.color[0]; green = swarm.color[1]; blue = swarm.color[2];
+        color = '#' + ((red << 16) | (green << 8) | blue).toString(16);
+        swarm_pos = new vec2(swarm.x, swarm.y);
+        $.each(swarm.members, function(i, particle) {
+            pos = swarm_pos.add(new vec2(particle.x, particle.y));
+            absolute_pos = pos.sub(viewport[0]);
+            drawParticle(ctx, absolute_pos, 5, color, 'black', 2, swarm.direction);
+        });
+    });
+
 }
 
 function initializeCanvas() {
@@ -19,16 +54,23 @@ function initializeCanvas() {
 	return ctx;
 }
 
-function drawCircle(ctx) {
-    ctx.clearRect(0, 0, game_canvas.width, game_canvas.height); // clear canvas
-	var centerX = game_canvas.width / 2;
-	var centerY = game_canvas.height / 2;
+function drawParticle(ctx, pos, radius, fillColor, borderColor, borderWidth, dir) {
+    dir = d2r(dir);
     ctx.beginPath();
-    ctx.arc(centerX, centerY, 10, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'green';
+    ctx.arc(pos.x, pos.y, radius, 0, 2*Math.PI, false);
+    ctx.fillStyle = fillColor;
     ctx.fill();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = '#003300';
+    ctx.lineWidth = borderWidth;
+    ctx.strokeStyle = borderColor;
+    ctx.stroke();
+
+    ctx.beginPath();
+    var pos2 = pos.add(new vec2(radius*Math.cos(dir), -radius*Math.sin(dir)));
+    ctx.arc(pos2.x, pos2.y, radius*.5, 0, 2*Math.PI, false);
+    ctx.fillStyle = fillColor;
+    ctx.fill();
+    ctx.lineWidth = borderWidth;
+    ctx.strokeStyle = borderColor;
     ctx.stroke();
 }
 
@@ -44,19 +86,80 @@ function initializeWebSocket(url, protocol, onmessage) {
     return ws;
 }
 
-function pingServer(ws) {
-    ws.send("U");
-    setTimeout(pingServer, 1000 / FPS / PINGS_PER_FRAME, ws);
+
+function pingServer(ws, ctx) {
+    if (PING_MODE == 'U') {
+        ws.send("U");
+    } else if (PING_MODE == 'viewport') {
+        viewport = getViewport(ctx);
+        obj = {viewport: [viewport[0].coords(), viewport[1].coords()]};
+        ws.send(JSON.stringify(obj));
+    }
+    setTimeout(pingServer, 1000 / FPS / PINGS_PER_FRAME, ws, ctx);
+
 }
 
 function getState() {
     return GLOBAL_STATE_NO_TOUCH;
 }
 
+function getID() {
+    return GLOBAL_ID_NO_TOUCH;
+}
+
+function getViewport(ctx) {
+    state = getState();
+    id = getID();
+    swarm = state.swarms[id]
+    center = new vec2(swarm.x, swarm.y);
+    screenSize = new vec2(ctx.canvas.width, ctx.canvas.height);
+    topLeft = center.sub(screenSize.times(.5));
+    bottomRight = center.add(screenSize.times(.5));
+    return [topLeft, bottomRight];
+}
+
 function vec2(x, y) {
     this.x = x;
     this.y = y;
-    this.magn = function() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    this.add = function(rhs) {
+        return new vec2(this.x+rhs.x,this.y+rhs.y);
     };
+    this.sub = function(rhs) {
+        return new vec2(this.x-rhs.x,this.y-rhs.y);
+    };
+    this.addX = function(rhs) {
+        return new vec2(this.x+rhs, this.y);
+    };
+    this.addY = function(rhs) {
+        return new vec2(this.x, this.y+addY);
+    };
+    this.magn = function() {
+        return Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2));
+    };
+    this.dir = function() {
+        return new vec2(this.x / this.magn(), this.y / this.magn());
+    };
+    this.times = function(rhs) {
+        return new vec2(this.x * rhs, this.y * rhs);
+    };
+    this.coords = function() {
+        return { x: this.x, y: this.y };
+    };
+}
+
+function _loop(ctx, dt, frameN) {
+    frameN++;
+    var before = performance.now();
+    frame(ctx, dt, frameN);
+    var frameT = performance.now() - before;
+    dt = (1000 / FPS) - frameT;
+    setTimeout(_loop, dt, ctx, dt, frameN);
+}
+
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function d2r(deg) {
+    return deg * Math.PI / 180;
 }
