2d6b477192baa080eef9c45cc05b65ab5541cf13
Benjamin Kulas <nazek42@ruby-reborn>
2018-01-27 01:20:09 -0600

    Initial netcode work and vector object
     - Created pingServer function which loops automatically to request new data
     - Created getState function which returns the global state without letting the requester touch it
     - Created vec2 object which will be useful for rendering

--- game.js
+++ game.js
@@ -1,12 +1,17 @@
+const SERVER_URL = "ws://159.203.171.13:8080";
+const PROTOCOL = "heroesoftheswarm";
+const FPS = 30;
+const PINGS_PER_FRAME = 1;
+var GLOBAL_STATE_NO_TOUCH;
+
 function main() {
-    const SERVER_URL = "ws://159.203.171.13:8080";
-    const PROTOCOL = "heroesoftheswarm";
-    ws = initialize_websocket(SERVER_URL, PROTOCOL);
-    ctx = initialize_canvas();
-    draw_circle(ctx);
+    var ws = initializeWebSocket(SERVER_URL, PROTOCOL, function(event) {GLOBAL_STATE_NO_TOUCH = event.data;});
+    setTimeout(pingServer, 500, ws);
+    var ctx = initializeCanvas();
+    drawCircle(ctx);
 }
 
-function initialize_canvas() {
+function initializeCanvas() {
 	// Get the canvas
 	var ctx = document.getElementById("game_canvas").getContext("2d");
 	// Set up the context
@@ -14,7 +19,7 @@ function initialize_canvas() {
 	return ctx;
 }
 
-function draw_circle(ctx) {
+function drawCircle(ctx) {
     ctx.clearRect(0, 0, game_canvas.width, game_canvas.height); // clear canvas
 	var centerX = game_canvas.width / 2;
 	var centerY = game_canvas.height / 2;
@@ -27,16 +32,31 @@ function draw_circle(ctx) {
     ctx.stroke();
 }
 
-function initialize_websocket(url, protocol) {
+function initializeWebSocket(url, protocol, onmessage) {
     // Initialize the websocket
     var ws = new WebSocket(url, protocol);
     // This is run when the server sends us something. It should handle all the different types of messages
-    ws.onmessage = function (event) {
-        console.log(event.data);
-    }
+    ws.onmessage = onmessage;
     // This is run when something fscks up
-    ws.onerror= function (event) {
+    ws.onerror = function (event) {
         console.log(event.data);
     }
     return ws;
+}
+
+function pingServer(ws) {
+    ws.send("U");
+    setTimeout(pingServer, 1000 / FPS / PINGS_PER_FRAME, ws);
+}
+
+function getState() {
+    return GLOBAL_STATE_NO_TOUCH;
+}
+
+function vec2(x, y) {
+    this.x = x;
+    this.y = y;
+    this.magn = function() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    };
 }
